a4c463d0b2b30ae6b60159646bf9fc0a28a34c6c
GUNEET SINGH <guneetsinghtuli@gmail.com>
2022-11-06 11:47:16 +0530

    sidebar

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ const App = () => {
 
             </TooltipComponent>
           </div>
-          {activeMenu && user != null ? (
+          {activeMenu ? (
             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
               <Sidebar />
             </div>
